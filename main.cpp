ea05beb3eaa5499eafa3a7340c214dbdb70bd321
camjenn2000 <117471210+camjenn2000@users.noreply.github.com>
2022-11-08 16:16:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-// write your code here
+#include <stdio.h>
+#include<math.h>
+int main
+{
+  printf("Enter a, b, and c: ");
+  double a,b,c;
+  scanf("%1f",&a);
+  scanf("%1f",&b);
+  scanf("%1f",&c);
+  if(b*b-4.0*a*c==0)
+  {
+    double x1=(b+pow(b*b-4.0*a*c,0.5))/(2.0*a)'
+    printf("The equation has 2 repeated roots.\n");
+    printf("Root is %f\n",x1);
+    }
+    else if(b*b-4*a*c>0)
+    {
+      double x1=(-b+pow(b*b-4.0*a*c,0.5))/(2.0*a);
+      double x2=(-b+pow(b*b-4.0*a*c,0.5))/(2.0*a);
+      printf("The equation has 2 different real roots.\n");
+      printf("Root is %f and %f\n",x1,x2);
+      }
